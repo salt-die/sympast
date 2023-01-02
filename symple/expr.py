4ef1e31de15a254bfbadf0570336cf6affe0709a
salt-die <53280662+salt-die@users.noreply.github.com>
2023-01-02 09:45:33 -0600

    Will try dataclasses for now.

--- symple/expr.py
+++ symple/expr.py
@@ -1,48 +1,22 @@
 __all__ = "Expr", "BinOp", "Var"
 
-from operator import itemgetter
+from dataclasses import dataclass
 
-_SYMBOL_MAP = dict(add="+", sub="-", mul="*", truediv="/")
 _EXPR_BODY = """
 def __{0}__(self, other):
     return BinOp('{1}', self, other)
 def __r{0}__(self, other):
     return BinOp('{1}', other, self)
 """
-_EXPR_NEW = """
-def __new__(cls, {0}):
-    return tuple.__new__(cls, ({0}))
-"""
+_SYMBOL_MAP = dict(add="+", sub="-", mul="*", truediv="/")
 
-class ExprMeta(type):
-    """
-    Expression metaclass.
-
-    Expressions are named tuples that respect inheritance.
-    """
-    def __new__(meta, name, bases, namespace):
-        args = tuple(namespace.get("__annotations__", ()))
-        exec(_EXPR_NEW.format(", ".join(args)), locals(), namespace)
-        namespace["__new__"].__defaults__ = tuple(namespace[arg] for arg in args if arg in namespace)
-
-        def __repr__(self):
-            return f"{name}({', '.join(f'{arg}={self[i]!r}' for i, arg in enumerate(args))})"
-
-        namespace |= {
-            '__slots__': (),
-            '__repr__': __repr__,
-            '__match_args__': args,
-        }
-        for index, arg in enumerate(args):
-            namespace[arg] = property(itemgetter(index), doc=f"Alias for field number {index}.")
-
-        return super().__new__(meta, name, bases, namespace)
-
-class Expr(tuple, metaclass=ExprMeta):
+@dataclass(frozen=True, slots=True)
+class Expr:
     for _item in _SYMBOL_MAP.items():
         exec(_EXPR_BODY.format(*_item))
     del _item
 
+@dataclass(frozen=True, slots=True)
 class BinOp(Expr):
     op: str
     lhs: Expr
@@ -53,6 +27,7 @@ class BinOp(Expr):
         rhs = f"({self.rhs})" if isinstance(self.rhs, BinOp) else f"{self.rhs}"
         return f"{lhs} {self.op} {rhs}"
 
+@dataclass(frozen=True, slots=True)
 class Var(Expr):
     name: str
 
