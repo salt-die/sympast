fb4fd104157b28b5f76c9778f7db271392eabc02
salt-die <53280662+salt-die@users.noreply.github.com>
2023-01-02 07:44:05 -0600

    We're not having fun if we're not meta programming.

--- symple/expr.py
+++ symple/expr.py
@@ -1,36 +1,60 @@
 __all__ = "Expr", "BinOp", "Var"
 
+from operator import itemgetter
+
 _SYMBOL_MAP = dict(add="+", sub="-", mul="*", truediv="/")
-_EXEC_STR = """
-def __{op}__(self, other):
-    return BinOp('{symbol}', self, other)
-def __r{op}__(self, other):
-    return BinOp('{symbol}', other, self)
+_EXPR_BODY = """
+def __{0}__(self, other):
+    return BinOp('{1}', self, other)
+def __r{0}__(self, other):
+    return BinOp('{1}', other, self)
+"""
+_EXPR_NEW = """
+def __new__(cls, {0}):
+    return tuple.__new__(cls, ({0}))
 """
 
+class ExprMeta(type):
+    """
+    Expression metaclass.
 
-class Expr:
-    for op, symbol in _SYMBOL_MAP.items():
-        exec(_EXEC_STR.format(op=op, symbol=symbol))
+    Expressions are named tuples that respect inheritance.
+    """
+    def __new__(meta, name, bases, namespace):
+        args = tuple(namespace.get("__annotations__", ()))
+        exec(_EXPR_NEW.format(", ".join(args)), locals(), namespace)
+        namespace["__new__"].__defaults__ = tuple(namespace[arg] for arg in args if arg in namespace)
 
+        def __repr__(self):
+            return f"{name}({', '.join(f'{arg}={self[i]!r}' for i, arg in enumerate(args))})"
+
+        namespace |= {
+            '__slots__': (),
+            '__repr__': __repr__,
+            '__match_args__': args,
+        }
+        for index, arg in enumerate(args):
+            namespace[arg] = property(itemgetter(index), doc=f"Alias for field number {index}.")
+
+        return super().__new__(meta, name, bases, namespace)
+
+class Expr(tuple, metaclass=ExprMeta):
+    for _item in _SYMBOL_MAP.items():
+        exec(_EXPR_BODY.format(*_item))
+    del _item
 
 class BinOp(Expr):
-    __match_args__ = "op", "lhs", "rhs"
-
-    def __init__(self, op, lhs, rhs):
-        self.op, self.lhs, self.rhs = op, lhs, rhs
+    op: str
+    lhs: Expr
+    rhs: Expr
 
     def __str__(self):
         lhs = f"({self.lhs})" if isinstance(self.lhs, BinOp) else f"{self.lhs}"
         rhs = f"({self.rhs})" if isinstance(self.rhs, BinOp) else f"{self.rhs}"
         return f"{lhs} {self.op} {rhs}"
 
-
 class Var(Expr):
-    __match_args__ = "name",
-
-    def __init__(self, name):
-        self.name = name
+    name: str
 
     def __str__(self):
         return self.name
